e22e0bd1a4afb922a7dfc6140a96944fdcc4b096
Jim Tang <jim.tang@lge.com>
2013-09-12 11:14:00 -0700

    Adding manifest file and updating nebulize.sh to use it

--- nebulize.sh
+++ nebulize.sh
@@ -2,75 +2,91 @@
 
 # nebulize.sh
 #
-# A bash shell script that uses Pandoc to convert all the markdown (.md) files
-# in the current directory (and its sudirectories) to HTML; the resulting
-# files are written to an output directory (./output). If the 'assets'
-# subdirectory exists, its contents are recursively copied to the output
-# directory.
+# A bash shell script that reads in a list of markdown (.md) files
+# from a manifest (outline.txt) and uses Pandoc to convert them to HTML;
+# the resulting files are written to an output directory (./output). If
+# the 'assets' subdirectory exists, its contents are copied to the
+# output directory.
 
-declare -a rawOutput
-declare -a directoryList
+docSourceRoot=( $(pwd) )
 
-basePath="$( cd "$( dirname "$0" )" && pwd )" 
-#echo $basePath
+cssFilePath=${docSourceRoot}/docs.css
+outputDir=${docSourceRoot}/output
+outlineFile=${docSourceRoot}/outline.txt
 
-rawOutput=( $(tree -dfi) )
+declare -a outlineArray
+index=0
+foundEmptyLine=0
 
-# echo ${rawOutput[@]}
-# echo ${#rawOutput[@]}
+# read in all lines from outline file, until we reach the end or an empty line
+while read line; do
+	if [ $((foundEmptyLine)) -eq 0 ]; then
+		myLine=$(echo $line)
+#		echo "myLine: $myLine"
+		strlen=$(echo ${#myLine})
+#		echo "strlen: $strlen"
+		if [ $strlen -gt "0" ]; then
+#			echo "index: $index"
+			outlineArray[$index]=$myLine
+			let index++
+		else
+#			echo "found empty line"
+			foundEmptyLine=1
+			break
+		fi
+	fi
+done <$outlineFile
+len=${#outlineArray[*]}
 
-for rawString in ${rawOutput[@]}
-do
-	# Only deal with tree output that begins with '.'
-	if [[ ${rawString:0:1} == "." ]]; then
-		# Exclude 'assets' and 'output' directories
-		if [[ ${rawString:1:7} != "/assets" && ${rawString:1:8} != "/assets/" && ${rawString:1:7} != "/output" && ${rawString:1:8} != "/output/" ]]; then
-			pathToTest=${basePath}${rawString:1}
-			if [[ -d ${pathToTest} ]]; then
-				directoryList=(${directoryList[@]} "$pathToTest")
+# remove, then recreate output directory
+rm -rf ${outputDir}
+mkdir -p ${outputDir}
+
+# create output subdirectories
+x=0
+previousNameToTest=
+while [ $x -lt $len ]; do
+	nameToTest=${outlineArray[$x]%/*.md}
+	if [[ $nameToTest != $previousNameToTest ]]; then
+		potentialDirName=$docSourceRoot/$nameToTest
+		if [ -d $potentialDirName ]; then
+			mkdir -p $outputDir/$nameToTest
+#			echo "creating directory ${outputDir}/${nameToTest}"
+		fi
+	fi
+	let x++
+	previousNameToTest=$nameToTest
+done
+
+# calculate output file paths and generate HTML files
+y=0
+while [ $y -lt $len ]; do
+	sourceFilePath=${docSourceRoot}/${outlineArray[$y]}
+	if [ -f ${sourceFilePath} ]; then
+		lenSourceFilePath=${#sourceFilePath}
+		pathExt=${sourceFilePath:lenSourceFilePath-3:3}
+		if [ ${pathExt} == ".md" ]; then
+			targetFilePath=$outputDir/${outlineArray[$y]%.md}.html
+#			echo "targetFilePath: ${targetFilePath}"
+			if [ -f $cssFilePath ]; then
+				# Create HTML file, pulling in stylesheet
+				pandoc -s -H $cssFilePath -f markdown -t html -o $targetFilePath $sourceFilePath
+			else
+				# Create HTML file without stylesheet
+				pandoc -s -f markdown -t html -o $targetFilePath $sourceFilePath
 			fi
 		fi
 	fi
-done
-
-outputBasePath=${basePath}/output
-cssFilePath=${basePath}/docs.css
-
-rm -rf ${outputBasePath}
-
-for sourceDir in ${directoryList[@]}
-do
-	# Determine whether this directory contains any .md files
-	mdFileCount="$( ls ${sourceDir}/*.md | wc -l )"
-	if [[ $mdFileCount -gt 0 ]]; then
-		outputDir=${sourceDir/$basePath/$outputBasePath}
-		mkdir -p $outputDir
-
-		cd ${sourceDir}
-		for mdFile in *.md
-		do
-			if [[ $mdFile != "README.md" ]]; then
-				# Calculate paths and generate HTML file
-				sourceFilePath=$sourceDir/$mdFile
-				targetFilePath=$outputDir/${mdFile%.md}.html
-				if [ -f $cssFilePath ]; then
-					pandoc -s -H $cssFilePath -f markdown -t html -o $targetFilePath $sourceFilePath
-				else
-					pandoc -s -f markdown -t html -o $targetFilePath $sourceFilePath
-				fi
-			fi
-		done
-	fi
+	let y++
 done
 
 # Copy over the assets directory, if it exists
 
-assetsSourceDir=${basePath}/assets
+assetsSourceDir=${docSourceRoot}/assets
 
 if [ -d $assetsSourceDir ]; then
-	assetsTargetDir=$outputBasePath/assets
+	assetsTargetDir=$outputDir/assets
 	mkdir -p $assetsTargetDir
 	cp $assetsSourceDir/* $assetsTargetDir/
 fi
-
 exit
