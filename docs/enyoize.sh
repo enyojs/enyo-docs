d1b2b693ea2fdc5809485bdbc76f9b477b5f2f69
Jim Tang <jim.tang@lge.com>
2014-10-20 14:17:10 -0700

    Adding 'enyoize' build customization script, along with header/footer files

---
+++ docs/enyoize.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# enyoize.sh
+#
+# A bash shell script that builds a set of docs and includes
+# the page header, footer and styles used on the enyojs.com
+# site.
+
+./build.sh --head _enyojs-head.html --header _enyojs-header.html --footer _enyojs-footer.html
+
+exit
