f710ee610a93752f2abf742ed4ecfb79b0140add
art.dahm <art.dahm@lge.com>
2014-11-20 18:21:35 -0800

    Initial integration of Bootplate into API documentation

--- tasks/copy-assets.js
+++ tasks/copy-assets.js
@@ -11,20 +11,25 @@ module.exports = function (grunt) {
 			
 		var cpy = function (from, to) {
 			fs.copySync(
-				path.resolve(process.cwd(), src, from),
-				path.resolve(process.cwd(), dest, to)
+				path.join(process.cwd(), src, from),
+				path.join(process.cwd(), dest, to)
 			);
 		};
-		
-		cpy('prism.css', 'css/prism.css');
+
+		cpy('css/prism.css', 'css/prism.css');
+		cpy('css/bootstrap.min.css', 'css/bootstrap.min.css');
+		cpy('css/bootstrap-theme.min.css', 'css/bootstrap-theme.min.css');
+
+		cpy('fonts/', 'fonts/');
 
 		// for js includes, we specify and install specific versions via bower but we
 		// arbitrarily map the correct include file into the output directory (for now)
 		cpy('js/jquery/dist/jquery.min.js', 'js/jquery.min.js');
 		cpy('js/jquery-hashchange/jquery.ba-hashchange.min.js', 'js/jquery.ba-hashchange.min.js');
-		cpy('main.js', 'js/main.js');
-		cpy('prism.js', 'js/prism.js');
-		
+		cpy('js/main.js', 'js/main.js');
+		cpy('js/prism.js', 'js/prism.js');
+		cpy('js/bootstrap.min.js', 'js/bootstrap.min.js');
+
 	});
 	
 };
