650fbcfb9b5553d2960d40848f047253fab05b1f
Roy Sutton <roy.sutton@lge.com>
2014-12-09 04:07:08 -0500

    Add task to substitute date on developer guide

--- Gruntfile.js
+++ Gruntfile.js
@@ -47,6 +47,27 @@ module.exports = function (grunt) {
 				files: {
 					src: 'output/**/*.html'
 				}
+			},
+			'date': {
+				options: {
+					pattern: /{{ generated }}/g,
+					replacement: function() {
+						// Yes, obviously this doesn't need to be calculated each time.  You fix it.
+						var d_names = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+						var m_names = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+						var d = new Date();
+						var curr_day = d.getDay();
+						var curr_date = ("0" + d.getDate()).substr(-2);
+						var curr_month = d.getMonth();
+						var curr_year = d.getFullYear();
+
+						return d_names[curr_day] + ", " + m_names[curr_month] + " " + curr_date + ", " + curr_year;
+					}
+				},
+				files: {
+					src: 'output/**/*.html'
+				}
 			}
 		},
 		
