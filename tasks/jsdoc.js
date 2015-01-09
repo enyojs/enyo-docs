c88467a19d681946f7f82e30b2c6c57f9b7c48db
Roy Sutton <roy.sutton@lge.com>
2015-01-09 17:29:19 -0500

    Update to use local install of jsdoc
    Enyo-DCO-1.1-Signed-off-by: Roy Sutton <roy.sutton@lge.com>

--- tasks/jsdoc.js
+++ tasks/jsdoc.js
@@ -6,7 +6,7 @@ module.exports = function (grunt) {
 		
 		var opts = grunt.config.get('jsdoc'),
 			done = this.async(),
-			args = ['-c'],
+			args = ['jsdoc', '-c'],
 			child;
 		
 		// push the conf file
@@ -16,8 +16,7 @@ module.exports = function (grunt) {
 		args.push('-d');
 		args.push(opts.dest || 'output');
 		
-		// TODO: update to use npm installed jsdoc
-		child = childProcess.spawn('jsdoc', args, {stdio: 'inherit'});
+		child = childProcess.spawn("./env.sh", args, {stdio: 'inherit'});
 
 		child.on('exit', done);
 		
